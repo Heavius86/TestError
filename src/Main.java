789c6665e87223c190666e8389f4526de9c36b79
Heavius86 <Heavius86@gmailcom>
2022-08-11 17:32:15 +0400

    first

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,16 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.sql.SQLOutput;
-import java.util.ArrayList;
-import java.util.InputMismatchException;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        String fileName = "src\\input.txt";
+        String fileName = "input.txt";
         List<String> list = new ArrayList<>();
         String[] splitStr;
 
@@ -21,39 +19,40 @@ public class Main {
             while((str = reader.readLine()) != null ){
                 if(!str.isEmpty()){
                     list.add(str);
-                    System.out.println(str);
+                   // System.out.println(str);
                 }
             }
         }
         catch (IOException e) {
             e.printStackTrace();
         }
-        for(String g : list ) {
-            splitStr = g.split(" ");
-            System.out.println(g);
-        };
 
 
-//        Scanner sc = new Scanner(System.in);
 
-//        try{
-//            double a = sc.nextDouble();
-//            String s = sc.next();
-//            double b = sc.nextDouble();
-//            System.out.println(res(a,s,b));
-//        }
-//        catch (InputMismatchException e){
-//            System.out.println("Error! Not number");
-//        }
-//        catch (ArithmeticException e ){
-//            System.out.println("Error! Division by zero");
-//        }
-//        catch (Exception e){
-//            System.out.println(e.getMessage());
-//        }
-//        finally {
-//            System.out.println("The end!");
-//        }
+        try{
+            for(String l : list ) {
+                splitStr = l.split(" ");
+                System.out.println(l);
+                double a = Double.valueOf(splitStr[0]);
+                String s = splitStr[1];
+                double b = Double.valueOf(splitStr[2]);
+                System.out.println(res(a,s,b));
+            };
+
+
+        }
+        catch (InputMismatchException e){
+            System.out.println("Error! Not number");
+        }
+        catch (ArithmeticException e ){
+            System.out.println("Error! Division by zero");
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        finally {
+            System.out.println("The end!");
+        }
     }
    static double res(double a, String s, double b ) throws Exception {
        double result = 0;
